a346e4a9619e4b7eb4245e777e8c04f9cf0c89a0
Liam Maeder <55049055+liammaeder@users.noreply.github.com>
2023-08-07 16:08:16 +0200

    removed unnecessary punctuation

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -46,7 +46,7 @@ const Tech = () => {
             <>
                 <motion.div variants={textVariant()}>
                     <p className={`${styles.sectionSubText}`}>My Skillset</p>
-                    <h2 className={`${styles.sectionHeadText}`}>Skills.</h2>
+                    <h2 className={`${styles.sectionHeadText}`}>Skills</h2>
                 </motion.div>
 
                 <div className="flex flex-row flex-wrap justify-center gap-10">
